1caeeb81088afb5a489b53cc4137698bcdd6ab38
Chaz Ferguson <chaz.ferguson@gmail.com>
2018-07-10 21:37:21 -0400

    update signin form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   wiki_root '/wiki'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'home#index'
 end
